7e5b23cc2e355f518b34822d39f0687b21f466a8
aissa-dev1 <aissadev1@gmail.com>
2025-01-21 23:04:07 +0100

    update home page create text logo card and text logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import TasksFilter from "@/components/pages/home/tasks-filter";
+import Container from "@/components/reusable/container";
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello todaydo!</p>
-    </div>
+    <Container>
+      <div className="bg-primary text-primary-foreground text-center mt-0 mb-4 sm:mt-4 py-8 px-4 rounded-none sm:rounded-md">
+        <h3 className="text-2xl font-bold uppercase">Today-do</h3>
+      </div>
+      <TasksFilter />
+    </Container>
   );
 }
